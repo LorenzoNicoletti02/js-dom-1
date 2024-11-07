1231cfc08f08eec85469a6194201352b53e37fd3
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-11-07 16:12:37 +0100

    feat: più impazzendo che brainstorming in js

---
+++ script.js
@@ -0,0 +1,8 @@
+const turnOn = document.querySelector(".off");
+const turnOff = document.querySelector(".on");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function() {
+    button.innerHTML = ("Spegni");
+    
+})
